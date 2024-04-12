650fd212c6b2c7af12054f7e7ae7a1a89ee282f9
Rhazes Devino <rhazesd@gmail.com>
2024-04-13 02:45:41 +0800

    feat(supplier) : create api for create new distributor and fetch master data

--- src/controllers/supplierController.js
+++ src/controllers/supplierController.js
@@ -3,29 +3,96 @@
 // });
 require("dotenv").config();
 const { pool, query } = require("../database");
+const moment = require("moment-timezone");
 
 const env = process.env;
 
 module.exports = {
   all: async (req, res) => {
     try {
-      let sqlQuery = `SELECT * FROM customers `;
-
-      const getCustomer = await query(
-        `SELECT *, customer_id AS id FROM customers;`
-      );
-      const getCountCustomer = await query(
-        `SELECT COUNT(*) AS totalCustomers FROM customers`
+      const getSupllier = await query(
+        `SELECT *, supplier_id AS id FROM suppliers;`
       );
 
       return res.status(200).send({
-        message: "Get Customer Data Success",
-        data: getCustomer,
-        total: getCountCustomer[0].totalCustomers,
+        message: "Get Supplier Data Success",
+        data: getSupllier,
       });
     } catch (error) {
-      console.error("Customer All Error:", error);
+      console.error("Supplier All Error:", error);
       res.status(500).send({ message: error });
+    }
+  },
+  master: async (req, res) => {
+    try {
+      const countTotalSupplier = await query(
+        `SELECT COUNT(*) AS totalSupplier FROM suppliers`
+      );
+      return res.status(200).send({
+        message: "Get Supplier Data Success",
+        data: {
+          totalSupplier: countTotalSupplier[0].totalSupplier,
+        },
+      });
+    } catch (error) {
+      console.error("Supplier All Error:", error);
+      res.status(500).send({ message: error });
+    }
+  },
+  masterDynamic: async (req, res) => {
+    try {
+      const masterSupplier = await query(
+        `SELECT supplier_id,supplier_name,supplier_code FROM suppliers`
+      );
+      return res.status(200).send({
+        message: "Get Master Dynamic Data Success",
+        data: {
+          supplier: masterSupplier,
+        },
+      });
+    } catch (error) {
+      console.error("Supplier All Error:", error);
+      res.status(500).send({ message: error });
+    }
+  },
+  create: async (req, res) => {
+    try {
+      const { supplierName, supplierCode } = req.body;
+
+      const existingSupplier = await query(
+        `SELECT COUNT(*) AS count FROM suppliers WHERE UPPER(supplier_code) = ?`,
+        [supplierCode.toUpperCase()]
+      );
+
+      if (existingSupplier[0].count > 0) {
+        return res
+          .status(400)
+          .send({ message: "Supplier code already exists" });
+      }
+      const uppercaseSupplierCode = supplierCode.toUpperCase();
+      const createdDate = moment
+        .tz("Asia/Jakarta")
+        .format("YYYY-MM-DD HH:mm:ss");
+
+      const result = await query(
+        `INSERT INTO suppliers (supplier_name, supplier_code, created_at) VALUES (?, ?, ?)`,
+        [supplierName, uppercaseSupplierCode, createdDate]
+      );
+
+      return res.status(200).send({
+        message: "Distributor created successfully",
+        data: {
+          supplierId: result.insertId,
+          supplierName,
+          supplierCode: uppercaseSupplierCode,
+          createdDate,
+        },
+      });
+    } catch (error) {
+      console.error("Create Distributor Error:", error);
+      return res
+        .status(500)
+        .send({ message: error.message || "Failed to create Distributor" });
     }
   },
 };
--- src/routes/supplierRouter.js
+++ src/routes/supplierRouter.js
@@ -4,5 +4,8 @@ const { supplierController } = require("../controllers");
 const router = express.Router();
 
 router.get("/all", supplierController.all);
+router.get("/master", supplierController.master);
+router.get("/master-dynamic", supplierController.masterDynamic);
+router.post("/create", supplierController.create);
 
 module.exports = router;
