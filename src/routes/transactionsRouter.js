644acb10a5561f7f0374e58ce307f845d62a2bb8
Rhazes Devino <rhazesd@gmail.com>
2024-04-14 01:56:55 +0800

    feat(supplier) : feat api for create and add master data

--- src/routes/transactionsRouter.js
+++ src/routes/transactionsRouter.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.get("/in/list", transactionsController.transactionInList);
 router.get("/in/detail/:id", transactionsController.transactionInDetail);
+router.post("/in/create", transactionsController.insertTransactionIn);
 
 module.exports = router;
