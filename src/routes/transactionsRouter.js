9dc4f9c491d03670356cd917c55ddfc8ec5e012d
Uzlah <landakuz@gmail.com>
2024-04-20 08:51:46 +0700

    nambahin api buat tachart~

--- src/routes/transactionsRouter.js
+++ src/routes/transactionsRouter.js
@@ -8,5 +8,7 @@ router.get("/in/detail/:id", transactionsController.transactionInDetail);
 router.post("/in/create", transactionsController.insertTransactionIn);
 router.get("/out/list/:id", transactionsController.transactionOutList);
 router.post("/out/create", transactionsController.insertTransactionOut);
+router.get("/out/report", transactionsController.transactionOutChrt);
+
 
 module.exports = router;
