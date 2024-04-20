94c0808e629b72c5907650f9ca261ad08d933e84
Rhazes Devino <rhazesd@gmail.com>
2024-04-20 19:55:21 +0800

    fix(product) : create controller for create,update,detail,delete

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,9 +1,5 @@
-// require("dotenv").config({
-//   path: ".env",
-// });
-require("dotenv").config();
 const { pool, query } = require("../database");
-
+const moment = require("moment-timezone");
 const env = process.env;
 
 module.exports = {
@@ -82,6 +78,181 @@ LIMIT 1;
     } catch (error) {
       console.error("Delete Product Error:", error);
       return res.status(500).send({ message: "Failed to delete Product" });
+    }
+  },
+  create: async (req, res) => {
+    try {
+      const {
+        name,
+        aklAkd,
+        expiredDate,
+        price,
+        stock,
+        productType,
+        productMerk,
+      } = req.body;
+
+      const createdDate = moment
+        .tz("Asia/Jakarta")
+        .format("YYYY-MM-DD HH:mm:ss");
+
+      const errors = [];
+      if (!name) {
+        errors.push({ field: "name", message: "Name is required" });
+      }
+      if (!aklAkd) {
+        errors.push({ field: "aklAkd", message: "NO AKL/AKD is required" });
+      }
+      if (!expiredDate) {
+        errors.push({
+          field: "expiredDate",
+          message: "Expired Date is required",
+        });
+      }
+      if (!price) {
+        errors.push({ field: "price", message: "Price is required" });
+      }
+
+      if (!productType) {
+        errors.push({
+          field: "productType",
+          message: "Product Type is required",
+        });
+      }
+      if (!productMerk) {
+        errors.push({
+          field: "productMerk",
+          message: "Product Merk is required",
+        });
+      }
+
+      if (errors.length > 0) {
+        return res.status(400).send({ errors });
+      }
+
+      const result = await query(
+        `INSERT INTO products (product_name, product_type, product_merk, akl_akd, price, stock , expired_date, created_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
+        [
+          name,
+          productType,
+          productMerk,
+          aklAkd,
+          price,
+          stock,
+          expiredDate,
+          createdDate,
+        ]
+      );
+
+      return res.status(200).send({
+        message: "Product created successfully",
+        data: result[0],
+      });
+    } catch (error) {
+      console.error("Create Product Error:", error);
+      return res
+        .status(500)
+        .send({ message: error.message || "Failed to create Product" });
+    }
+  },
+  update: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const {
+        name,
+        aklAkd,
+        expiredDate,
+        price,
+        stock,
+        productType,
+        productMerk,
+      } = req.body;
+      const updatedDate = moment
+        .tz("Asia/Jakarta")
+        .format("YYYY-MM-DD HH:mm:ss");
+
+      const errors = [];
+      if (!id) {
+        errors.push({ field: "id", message: "ID is required" });
+      }
+      if (!name) {
+        errors.push({ field: "name", message: "Name is required" });
+      }
+      if (!aklAkd) {
+        errors.push({ field: "aklAkd", message: "Akl Akd is required" });
+      }
+      if (!expiredDate) {
+        errors.push({
+          field: "expiredDate",
+          message: "Expired Date is required",
+        });
+      }
+      if (!price) {
+        errors.push({ field: "price", message: "Price is required" });
+      }
+
+      if (!productType) {
+        errors.push({
+          field: "productType",
+          message: "Product Type is required",
+        });
+      }
+      if (!productMerk) {
+        errors.push({
+          field: "productMerk",
+          message: "Product Merk is required",
+        });
+      }
+
+      if (errors.length > 0) {
+        return res.status(400).send({ errors });
+      }
+
+      const updateProduct = await query(
+        `UPDATE products 
+       SET product_name = ?, product_type  = ?, product_merk  = ?, akl_akd  = ?, price  = ?, stock  = ? , expired_date  = ?  , updated_at = ?
+       WHERE product_id = ?`,
+        [
+          name,
+          productType,
+          productMerk,
+          aklAkd,
+          price,
+          stock,
+          expiredDate,
+          updatedDate,
+          id,
+        ]
+      );
+
+      if (updateProduct.affectedRows === 0) {
+        return res.status(404).send({ message: "Product not found" });
+      }
+
+      return res.status(200).send({
+        message: "Product updated successfully",
+      });
+    } catch (error) {
+      console.error("Product Update Error:", error);
+      res.status(500).send({ message: "Failed to update Product" });
+    }
+  },
+  detail: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const getProduct = await query(
+        `SELECT * FROM products
+        WHERE product_id = ${id}
+        `
+      );
+
+      return res.status(200).send({
+        message: "Get Product Detail Success",
+        data: getProduct[0],
+      });
+    } catch (error) {
+      console.error("Product Detail Error:", error);
+      res.status(500).send({ message: error });
     }
   },
 };
--- src/routes/productRouter.js
+++ src/routes/productRouter.js
@@ -6,5 +6,8 @@ const router = express.Router();
 router.get("/all", productController.all);
 router.get("/master", productController.master);
 router.delete("/delete/:id", productController.delete);
+router.post("/create", productController.create);
+router.put("/update/:id", productController.update);
+router.get("/detail/:id", productController.detail);
 
 module.exports = router;
