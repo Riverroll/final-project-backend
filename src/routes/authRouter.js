ba44c3ee5ae41927738c3eb81f6dbafb0f919a0a
Rhazes Devino <rhazesd@gmail.com>
2024-04-22 23:40:47 +0800

    feat(user) : feat user controller and role controller

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -8,5 +8,7 @@ router.post("/login", authControllers.login);
 router.post("/register", authControllers.register);
 router.put("/update/user", authControllers.editUser);
 router.delete("/delete/user/:user_id", authControllers.deleteUser);
+router.get("/user/all", authControllers.allUser);
+router.get("/role/all", authControllers.allRole);
 
 module.exports = router;
